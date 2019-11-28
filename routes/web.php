5ad3240ce38368c4df713215f95497d596f234ae
Javion <t832566751@gmail.com>
2019-11-29 02:04:35 +0800

    建立 routes

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,19 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Auth::routes();
 
-Route::get('/dashboard/home', 'DashboardController@versionone')->name('home');
-Route::get('/dashboard/v2', 'DashboardController@versiontwo')->name('v2');
-Route::get('/dashboard/v3', 'DashboardController@versionthree')->name('v3');
+Route::get('/home', 'DashboardController@index')->name('home');
+Route::get('/patient', 'DashboardController@patient')->name('patient');
+Route::get('/doctor', 'DashboardController@doctor')->name('doctor');
+Route::get('/ward', 'DashboardController@ward')->name('ward');
+Route::get('/operation', 'DashboardController@operation')->name('operation');
+
+
+// Route::get('/dashboard/v2', 'DashboardController@versiontwo')->name('v2');
+// Route::get('/dashboard/v3', 'DashboardController@versionthree')->name('v3');
 
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');

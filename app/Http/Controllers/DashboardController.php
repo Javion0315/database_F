7066205a80ffae5bcd50b0e173ffd892c098b93b
Javion <t832566751@gmail.com>
2019-11-29 02:08:37 +0800

    controller 取得資料回傳到 view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Doctor;
+use App\Operation;
+use App\Patient;
+use App\Ward;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function versionone()
+    public function index()
     {
-        return view('dashboard.v1');
+        return view('home');
     }
     public function versiontwo()
     {
@@ -17,5 +21,29 @@ class DashboardController extends Controller
     public function versionthree()
     {
         return view('dashboard.v3');
+    }
+
+    public function patient()
+    {
+        $patient = Patient::All();
+        return view('patient', ['patient' => $patient]);
+    }
+
+    public function doctor()
+    {
+        $doctor = Doctor::All();
+        return view('doctor', ['doctor' => $doctor]);
+    }
+
+    public function ward()
+    {
+        $ward = Ward::All();
+        return view('ward', ['ward' => $ward]);
+    }
+
+    public function operation()
+    {
+        $operation = Operation::All();
+        return view('operation', ['operation' => $operation]);
     }
 }
